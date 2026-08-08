14f7e78b38e6a130cadd5e8a74398c51cb1f1c0d
Moksh0008 <mokshithraghavanjaneya@gmail.com>
2026-08-08 20:07:50 +0530

    Complete AniMaker Version 1.0

---
+++ js/auth.js
@@ -0,0 +1,30 @@
+// Login
+const loginForm = document.getElementById("login-form");
+
+if (loginForm) {
+    loginForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        window.location.href = "home.html";
+    });
+}
+
+
+// Sign Up
+const signupForm = document.getElementById("signup-form");
+
+if (signupForm) {
+    signupForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        let password = document.getElementById("signup-password").value;
+        let confirmPassword = document.getElementById("signup-confirm-password").value;
+
+        if (password !== confirmPassword) {
+            alert("Passwords do not match!");
+            return;
+        }
+
+        window.location.href = "home.html";
+    });
+}   
